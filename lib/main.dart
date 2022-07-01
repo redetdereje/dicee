818c04482e7f3923a4f4e2f647a5bdefaf89ccfe
redetdereje <redetdereje91@gmail.com>
2022-07-01 17:31:48 -0400

    first commit - finished app

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int LeftDiceNum = 1;
+  int RightDiceNum = 1;
+  void change_dice() {
+    setState(() {
+      LeftDiceNum = Random().nextInt(6) + 1;
+      RightDiceNum = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  change_dice();
+                },
+                child: Image.asset('images/dice$LeftDiceNum.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  change_dice();
+                },
+                child: Image.asset('images/dice$RightDiceNum.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
